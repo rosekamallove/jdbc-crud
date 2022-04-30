6dca05a3ddbebb38ccd0bc71fc0ebb4e944af558
Rose Kamal Love <rosekamallove@gmail.com>
2022-04-30 20:55:07 +0530

    insert data

---
+++ insert_db.sql
@@ -0,0 +1,7 @@
+insert into engineeringstudents value(10201, 'CSE', 'Kiran', 'Acharya', 2018, 1272);
+insert into engineeringstudents value(10202, 'ME', 'Jayson', 'Robert', 2020, 1249);
+insert into engineeringstudents value(10203, 'CSE', 'Praveen', 'Kumar', 2017, 1238);
+insert into engineeringstudents value(10204, 'ECE', 'Rajesh', 'Kumar', 2019, 1243);
+insert into engineeringstudents value(10205, 'CSE', 'Trevor', 'Krugar', 2018, 1000);
+insert into engineeringstudents value(10206, 'CSE', 'Jogn', 'Denver', 2018, 1);
+insert into engineeringstudents value(10207, 'ECE', 'Jennifer', 'Charles', 2019, 13232);
